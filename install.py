1c4817dfce3fe19ba317f1751a47c271ce4ee54d
Alperen <alperen.ergel@yandex.com>
2020-05-28 20:50:54 +0300

    Update install.py

--- install.py
+++ install.py
@@ -9,6 +9,7 @@ print("\n" + W + "\033[1;35;1mRequired Applications Downloading\n" + ENDC)
  
 os.system("apt-get update")
 os.system("apt-get -y install git")
+os.system("apt-get -y install python-pip")
 os.system("apt-get -y install python3")
 os.system("apt-get -y install ")
 os.system("apt-get -y install nmap")
